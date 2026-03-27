5d0d1a59b23e9f7139efe16ac3de2bfa01fbb246
Eder Oliveira <ederoliveira152@gmail.com>
2026-03-27 18:59:43 -0300

    hide navbar logo

--- script/script.js
+++ script/script.js
@@ -53,3 +53,15 @@
         document.querySelectorAll('.service-card, .gallery-item').forEach(el => {
             observer.observe(el);
         });
+
+        // Header scroll effect
+        const header = document.querySelector('header');
+        if (header) {
+            window.addEventListener('scroll', () => {
+                if (window.scrollY > 50) {
+                    header.classList.add('scrolled');
+                } else {
+                    header.classList.remove('scrolled');
+                }
+            });
+        }
